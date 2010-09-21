006a3cd9d611648b637d30181e60c77732a24985
Jason R. Coombs <jaraco@jaraco.com>
2010-09-21 08:50:08 -0400

    Restored threaded test

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -2,20 +2,20 @@ from threading import Thread
 import py.test
 
 import test_evp
+import test_cipher
 
 class ThreadedTester(Thread):
 	failed = False
 	def run(self):
 		try:
 			test_evp.test_digest()
-			test_evp.test_cipher(['a'*1000, 'd'*1000])
-			test_evp.test_rand()
+			test_cipher.test_cipher(['a'*1000, 'd'*1000])
+			#test_evp.test_rand()
 		except Exception, e:
 			self.failed = True
 			self.exception = e
 
 def test_threaded_crypto():
-	py.test.skip('currently fails')
 	threads = [ThreadedTester() for i in range(10)]
 	map(lambda t: t.start(), threads)
 	# wait for the threads to complete
